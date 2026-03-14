2ad944eb93b55c22aac3ffddd95b56e5f0d0072f
Unique2112 <uniquendris7@gmail.com>
2026-03-14 14:27:04 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from crm_app import views 
-
 urlpatterns = [
-    # ይህ መስመር የግድ መኖር አለበት
     path('print-proforma/<int:pk>/', views.print_proforma, name='print_proforma'),
-    
     path('dashboard/', views.dashboard, name='dashboard'),
     path('admin/', admin.site.urls),
 ]
