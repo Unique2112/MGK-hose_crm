bdcd82906978004bc83ecf6877889024a64f7c96
Unique2112 <uniquendris7@gmail.com>
2026-03-16 16:32:44 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path
 from crm_app import views 
-
 urlpatterns = [
     # ይህ መስመር የግድ መኖር አለበት
     path('print-proforma/<int:pk>/', views.print_proforma, name='print_proforma'),
