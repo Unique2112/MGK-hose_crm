9d693acfcd8e948d9f30c31011a7f325635bb3c5
Unique2112 <uniquendris7@gmail.com>
2026-03-17 12:34:21 +0300

    Update admin.py

--- crm_app/admin.py
+++ crm_app/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import Category, Product, Customer, Proforma
 
-# እነዚህን ሞዴሎች በ Admin Panel ላይ እንዲታዩ መመዝገብ
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'stock_quantity') # እነዚህን ሰንጠረዡ ላይ ያሳያል
+    list_filter = ('category',) # በካታጎሪ እንድትለይ ይረዳሃል
+
 admin.site.register(Category)
-admin.site.register(Product)
 admin.site.register(Customer)
 admin.site.register(Proforma)
