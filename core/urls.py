6069584926e589cc30747c30218de40cabe9ddfa
Unique2112 <uniquendris7@gmail.com>
2026-03-30 11:07:36 +0300

    Delete core directory

--- core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from crm_app import views 
-urlpatterns = [
-    path('print-proforma/<int:pk>/', views.print_proforma, name='print_proforma'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('admin/', admin.site.urls),
-]
